010a7a2627d1868dad773c2c5e2311511680d064
tops-bhadresh <tops.bhadresh@gmail.com>
2019-01-20 23:02:53 -0800

    upload

--- actvity_progressbar/ViewController.swift
+++ actvity_progressbar/ViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var actIndi: UIActivityIndicatorView!
+  
+    @IBOutlet weak var progressView1: UIProgressView!
+    
+    var timer:Timer!
+    
+    @IBAction func StartProcessAction(_ sender: UIButton) {
+
+        actIndi.startAnimating()
+        
+    }
+    
+    @IBAction func StopProcessAction(_ sender: UIButton) {
+        
+        actIndi.stopAnimating()
+    }
+    
+    var c:Float = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        actIndi.hidesWhenStopped = true
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
+            print("hi")
+            if self.c == 1
+            {
+                timer.invalidate()
+            }
+            self.progressView1.progress = self.c
+
+            self.c += 0.1
+        })
+    
+        timer.fire()
+        
     }
 
     override func didReceiveMemoryWarning() {
